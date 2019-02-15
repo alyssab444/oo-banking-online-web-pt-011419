a42e4af4db8bf32741ab9f2d3daaeb341991ad61
Alyssa Brown <alyssa.brown@live.com>
2019-02-15 01:50:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,13 @@
 class Transfer
    attr_accessor :sender, :receiver, :amount, :status
+  
   def initialize(sender, receiver, amount)
     @sender= sender 
     @receiver= receiver
     @amount= amount
     @status= "pending"
   end 
+  
   def valid?
     if @sender.valid? == true && @reciver.valid? == true && @sender.balance >= @amount 
      return true 
