dce01e73fe623a32dd5b89dbbcc67dac8ac0e569
Alyssa Brown <alyssa.brown@live.com>
2019-02-15 01:30:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
    attr_accessor :sender, :receiver, :amount 
   def initialize(sender, receiver, amount)
-    @sender= @sender
+    @sender= sender 
     @receiver= receiver
     @amount= amount
     
