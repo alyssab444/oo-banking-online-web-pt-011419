9c88d65c72096e0eab9f82bd6d076a7d38f0c8a9
Alyssa Brown <alyssa.brown@live.com>
2019-02-15 00:58:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
   attr_accessor :name, :balance, :status
-  def initialize(name, balance, status)
+  def initialize(name, balance= 1000, status= "open")
     @name=name
-    @balance= 1000
-    @status= "open"
+    @balance= balance 
+    @status= status
   end 
 end
